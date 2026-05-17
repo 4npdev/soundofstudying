a89746592c94ef9e534c6cc1fabc9de23dd3d4df
4npweb <4np.web@gmail.com>
2026-05-18 01:23:18 +0200

    Added Javascript

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 let buttons = document.querySelectorAll(".sound-btn");
+const sounds = [
+    "assets/rain.mp3",
+    "assets/thunder.mp3",
+    "assets/city.mp3",
+    "assets/crickets.mp3",
+    "assets/lofi.mp3"
+]
 
-buttons.forEach((btn) => {
+buttons.forEach((btn, index) => {
+    const audio = new Audio(sounds[index]);
+    audio.loop = true;
+
     btn.addEventListener('click', () => {
         btn.classList.toggle("active");
+
+        if(audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+            audio.currentTime = 0;
+        }
     })
+
+    
 })
